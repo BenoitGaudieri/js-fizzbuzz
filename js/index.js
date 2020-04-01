66ea5c1c4b93b7adf75801057754cb0e0476d413
BenoitGaudieri <benalek@gmail.com>
2020-04-01 13:06:11 +0200

    feat. done?

---
+++ js/index.js
@@ -0,0 +1,15 @@
+var result = "";
+
+for (i = 1; i < 101; i++) {
+    if (i % 2 != 0 && i % 5 == 0) {
+        result += " FizzBuzz ";
+    } else if (i % 5 == 0) {
+        result += " Buzz ";
+    } else if (i % 2 != 0) {
+        result += " Fizz ";
+    } else {
+        result += " " + i + " ";
+    }
+}
+
+console.log(result);
