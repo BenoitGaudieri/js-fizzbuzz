531935288de37255cc4f31c238d9cd513a607e19
BenoitGaudieri <benalek@gmail.com>
2020-04-01 13:13:53 +0200

    feat. done. probably.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 var result = "";
 
 for (i = 1; i < 101; i++) {
-    if (i % 2 != 0 && i % 5 == 0) {
+    if (i % 3 == 0 && i % 5 == 0) {
         result += " FizzBuzz ";
     } else if (i % 5 == 0) {
         result += " Buzz ";
-    } else if (i % 2 != 0) {
+    } else if (i % 3 == 0) {
         result += " Fizz ";
     } else {
         result += " " + i + " ";
