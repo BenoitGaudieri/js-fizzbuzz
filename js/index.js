db3eae1c9ed9cc3f41adb71c7b0f049dc3175a5d
BenoitGaudieri <benalek@gmail.com>
2020-04-01 19:53:15 +0200

    feat. style

--- js/index.js
+++ js/index.js
@@ -2,15 +2,21 @@ var result = "";
 
 for (i = 1; i < 101; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
-        result += " FizzBuzz, ";
+        result += "<span class='gradient--red'>FizzBuzz</span>";
     } else if (i % 5 == 0) {
-        result += " Buzz, ";
+        result += "<span class='gradient--blue'>Buzz</span>";
     } else if (i % 3 == 0) {
-        result += " Fizz, ";
+        result += "<span class='gradient--green'>Fizz</span>";
     } else {
-        result += " " + i + ", ";
+        result += i;
+    }
+    while (i != 100) {
+        result += "<span class='gradient--gray'>, </span>";
+        break;
     }
 }
 
 console.log(result);
-document.getElementById("output").innerHTML = result;
+document.getElementById(
+    "output"
+).innerHTML = `<p class='bounce too-much'>${result}</p>`;
